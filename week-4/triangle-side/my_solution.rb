10b2cf4cd7cdb05e864d06f1d8c121b0be16809b
Logan Bresnahan <loganbbres@gmail.com>
2015-10-21 18:10:23 -0500

    Finish 4.5.3

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -3,6 +3,20 @@
 
 # Your Solution Below
 
+
 def valid_triangle?(a, b, c)
-  # Your code goes here!
+x = (a * b) * 0.5
+y = (a * c) * 0.5
+z = (b * c) * 0.5
+  if (a <= 0) || (b <= 0) || (c <= 0)
+    false
+  else
+    if (x == y) && (y == z) && (x == z)
+      true
+  	elsif (a + b) > (c) && (c + b) > (a) && (a + c) > (b)
+      true
+    else
+      false
+    end
+  end
 end
