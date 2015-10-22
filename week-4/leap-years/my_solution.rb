9e8da3c9457ddd460529b11f7be7eac073564488
Logan Bresnahan <loganbbres@gmail.com>
2015-10-22 13:08:46 -0500

    Finish 4.5.4

--- week-4/leap-years/my_solution.rb
+++ week-4/leap-years/my_solution.rb
@@ -4,3 +4,17 @@
 
 
 # Your Solution Below
+
+def leap_year?(year)
+if (year % 4 == 0)
+	if (((year % 4 == 0) && (year % 100 == 0)) && (year % 400 != 0))
+		false
+	else (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
+		true
+	end
+else
+	false
+end
+end
+
+leap_year?(2000)
