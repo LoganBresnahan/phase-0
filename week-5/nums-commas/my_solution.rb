8294dad61cf7472159dcb5a0d1307b551e062eb5
Logan Bresnahan <loganbbres@gmail.com>
2015-10-28 10:15:47 -0500

    5.5 does not work for numbers 32 digits or higher.

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -24,22 +24,33 @@ def separate_commas(integer)
 	x = 2
 	split_i = integer.to_s.reverse#.insert(i + 3, ",").reverse#.split("").join
 	i_leng = split_i.length
-	while i < i_leng
+	while i <= i_leng
+
+		if i_leng <= 3
+			p integer.to_s
+			exit
+		end
+
+		if i_leng == 4 || i_leng == 5 || i_leng == 6
+			split_i.insert(3, ",")
+			p split_i.reverse
+			exit
+		end
+
 		x += 1
+
 		split_i.insert(i += x, ",")
-		#x -= 1
-		#p split_i.reverse
-			if x == 4
-				x = 3
-			end
-		#total_size = split_i.insert(etc)
-		#break if total_size >= split_i.length
-		break if(i == i_leng + 1) || (i == i_leng + 2)
+
+		if x == 4
+			x = 3
+		end
+
+		#break if(i == i_leng + 1) || (i == i_leng + 2) || (i == i_leng - 1) #|| (i % i_leng == 0)
 	end
 	p split_i.reverse
 end 
 
-separate_commas(100000)
+separate_commas(1000000000000000000000000)
 
 #if i + 3 > split_i.length then
 #	i+=3, ","
