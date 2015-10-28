a54ec86ebe33f1cdaa24b0bcf82cccdd55a6d8ac
Logan Bresnahan <loganbbres@gmail.com>
2015-10-28 13:58:38 -0500

    Finish working solution for rspec on 5.3. Reflection not done

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,3 +1,4 @@
+=begin
 # Calculate the mode Pairing Challenge
 
 # I worked on this challenge [by myself, with: ]
@@ -13,17 +14,43 @@
 # 0. Pseudocode
 
 # What is the input?
+an array
+
 # What is the output? (i.e. What should the code return?)
+An array of most frequently used objects in the initial array. No duplicate values.
+
 # What are the steps needed to solve the problem?
 
+What are the steps needed to solve the problem?
 
+identify a method that would sort the array
+
+identify the number of times each object in the array is used, and select only those which are used the most often.
+
+refine the array such that each "mode" item is used only once.
+
+return the new/modified array
+=end
 # 1. Initial Solution
-
+def mode(array)
+  max_frequency = 0
+  array.each do |x|
+    max_frequency = array.count(x) if array.count(x) > max_frequency
+  end
+  array.delete_if do |x|
+    array.count(x) < max_frequency
+  end
+  array.uniq!
+end
 
 
 
 # 3. Refactored Solution
-
+def mode(array)
+  max_frequency = 0
+  array.each {|x| max_frequency = array.count(x) if array.count(x) > max_frequency}
+  array.delete_if {|x| array.count(x) < max_frequency}.uniq!
+end
 
 
 
