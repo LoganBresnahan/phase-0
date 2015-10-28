b182c4b963a31a07d55de828d5364b3bd47631e1
Logan Bresnahan <loganbbres@gmail.com>
2015-10-27 22:17:12 -0500

    Have comma put into number but incorect for higher numbers, for 5.5

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -19,13 +19,80 @@ Return the new modified parameter.
 =end
 
 # 1. Initial Solution
+def separate_commas(integer)
+	i = 0
+	x = 2
+	split_i = integer.to_s.reverse#.insert(i + 3, ",").reverse#.split("").join
+	p split_i
+	while i < split_i.length
+		x += 1
+		split_i.insert(i += x, ",")
+		#x -= 1
+		p split_i.reverse
+		#total_size = split_i.insert(etc)
+		#break if total_size >= split_i.length
+		break if(i == split_i.length + 1) || (i == split_i.length + 2)
+	end
+	p split_i.reverse
+end 
 
+separate_commas(1000000000)
 
-
-
+#if i + 3 > split_i.length then
+#	i+=3, ","
 # 2. Refactored Solution
-
+=begin
+i = 0
+while i <= split_i.length
+i + 3
+split_i.insert(i + 3, ",")
+split_i.reverse
+#add comma in right place
+#insert(-4 + 3, ",")
+=end
 
 
 
 # 3. Reflection
+
+# if 3 spaces exist from the right put a comma
+
+#.reverse to work from the left
+=begin
+if split_i.length % 3 == 1
+		p split_i
+end
+
+
+if split_i[2].empty? == false
+	slice[, 2]
+end
+
+
+if unknown % 3 == 1
+	then special_space = true
+end
+
+
+if special_space == true
+	then slice in comma
+end
+
+#****
+if split_i[space/self] % 3 == 1
+	then slice [, space]
+end
+
+.to_h.key % 3 == 1
+then push , 
+
+for every three items in an array add space if space.empty? then space == [,]
+
+
+
+comma_array = [","]
+while true
+	if split_i[space] != ","
+new_arrays = split_i.take(3)
+new_arrays + comma_array
+=end
