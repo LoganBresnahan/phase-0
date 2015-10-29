99b8bb22e51430c28eda129f8910e11c589ae61e
Logan Bresnahan <loganbbres@gmail.com>
2015-10-28 22:16:13 -0500

    Create fully working 5.5 but fails part of rspec due to special return from method

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -19,61 +19,52 @@ Return the new modified parameter.
 =end
 
 # 1. Initial Solution
-def separate_commas(integer)
+def separate_comma(integer)
 	i = 0
 	x = 2
+	fake = " "
 	split_i = integer.to_s.reverse#.insert(i + 3, ",").reverse#.split("").join
 	i_leng = split_i.length
 	while i <= i_leng
 		
 		if i_leng <= 3
 			p integer.to_s
-			exit
+			return
 		end
 
 		if i_leng == 4 || i_leng == 5 || i_leng == 6
 			split_i.insert(3, ",")
 			p split_i.reverse
-			exit
+			return
 		end
-
+		
 		x += 1
+		split_i << fake
 		split_i.insert(i += x, ",")
-
+		
 		if x == 4
 			x = 3
 		end
-
-		break if(i == i_leng + 1) || (i == i_leng + 2) || (i == i_leng - 1) #|| (i % i_leng == 0)
+		
+		#break if(i == i_leng + 1) || (i == i_leng + 2) || (i == i_leng - 1) #|| (i % i_leng == 0)
 	end
+
+	split_i.strip!
+
 	if split_i.reverse.start_with?(",")
 		corr_dir = split_i.reverse
 		corr_dir[0] = ""
-		p corr_dir
-	else
+		p corr_dir.strip
+	elsif 
 		p split_i.reverse
 	end
 end 
 
-separate_commas(1000000)
+separate_comma(100)
 
-#if i + 3 > split_i.length then
-#	i+=3, ","
-# 2. Refactored Solution
-=begin
-i = 0
-while i <= split_i.length
-i + 3
-split_i.insert(i + 3, ",")
-split_i.reverse
-#add comma in right place
-#insert(-4 + 3, ",")
-=end
+
+
 
 
 
 # 3. Reflection
-
-# if 3 spaces exist from the right put a comma
-
-#.reverse to work from the left
