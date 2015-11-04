5c41c812b6ef89d5e786d52089ceb09bb7ba0ad8
Logan Bresnahan <loganbbres@gmail.com>
2015-11-04 17:55:03 -0600

    Add board display to bingo challenge

--- week-6/bingo-solution.rb
+++ week-6/bingo-solution.rb
@@ -14,27 +14,72 @@
   that was called was in the bingo board.
 -I need to display the board after each churn and make it pretty.
 =end
+=begin
 # Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
   #fill in the outline here
+	def call_letter_number
+		
+	end
 
 # Check the called column for the number called.
   #fill in the outline here
+  	def check_letter_number
+		
+  	end
 
 # If the number is in the column, replace with an 'x'
   #fill in the outline here
+  	if number is in column
+  		replace with "x"
+  	end
 
 # Display a column to the console
   #fill in the outline here
+  	puts new_column
 
 # Display the board to the console (prettily)
   #fill in the outline here
-
+    puts "*============*"
+    puts "B |I |N |G |O "
+  	puts "______________"
+  	puts "#{board[0][0]}|#{board[0][1]}|#{board[0][2]}|#{board[0][3]} |#{board[0][4]}"
+  	puts "______________"
+  	puts "#{board[1][0]}|#{board[1][1]}|#{board[1][2]}|#{board[1][3]}|#{board[1][4]}"
+  	puts "______________-"
+  	puts "#{board[2][0]}|#{board[2][1]}|#{board[2][2]}|#{board[2][3]}|#{board[2][4]}"
+  	puts "______________"
+  	puts "#{board[3][0]}|#{board[3][1]}|#{board[3][2]}|#{board[3][3]}|#{board[3][4]}"
+  	puts "______________"
+  	puts "#{board[4][0]}|#{board[4][1]}|#{board[4][2]}|#{board[4][3]}|#{board[4][4]}"
+  	puts "*============*"
+=end
 # Initial Solution
 
 class BingoBoard
 
   def initialize(board)
     @bingo_board = board
+    @display =  puts "*============*"
+			    puts "B |I |N |G |O "
+			  	puts "______________"
+			  	puts "#{board[0][0]}|#{board[0][1]}|#{board[0][2]}|#{board[0][3]} |#{board[0][4]}"
+			  	puts "______________"
+			  	puts "#{board[1][0]}|#{board[1][1]}|#{board[1][2]}|#{board[1][3]}|#{board[1][4]}"
+			  	puts "______________-"
+			  	puts "#{board[2][0]}|#{board[2][1]}|#{board[2][2]}|#{board[2][3]}|#{board[2][4]}"
+			  	puts "______________"
+			  	puts "#{board[3][0]}|#{board[3][1]}|#{board[3][2]}|#{board[3][3]}|#{board[3][4]}"
+			  	puts "______________"
+			  	puts "#{board[4][0]}|#{board[4][1]}|#{board[4][2]}|#{board[4][3]}|#{board[4][4]}"
+			  	puts "*============*"
+  end
+
+  def call_letter_number
+  
+  end
+
+  def check_letter_number
+
   end
 
 
@@ -50,6 +95,7 @@ board = [[47, 44, 71, 8, 88],
         [83, 85, 97, 89, 57],
         [25, 31, 96, 68, 51],
         [75, 70, 54, 80, 83]]
+
 
 new_game = BingoBoard.new(board)
 
