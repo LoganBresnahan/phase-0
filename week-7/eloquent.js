eefc690cd32de53637e5f0f879b9204bb25d0fc2
Logan Bresnahan <loganbbres@gmail.com>
2015-11-10 11:49:00 -0600

    Add eloquent.js to week-7 and finish intro and ch. 1 questions for 7.2

---
+++ week-7/eloquent.js
@@ -0,0 +1,61 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+
+
+
+
+
+// Text Questions
+// Introduction
+// -Did you learn anything new about JavaScript or programming in general?
+// 	Yes, I don't really know much about it at all. He says that it is very
+// 	liberal which scares me. I love throwing code at the interpreter to see if 
+// 	it works, but I imagine I will need to be more careful with JavaScript.
+
+// -Are there any concepts you feel uncomfortable with?
+// 	As of now no, I'm sure I will when I start to really use it. I'm looking forward
+// 	to using JS as I imagine it will force me to be a little more logical and careful with
+// 	my code vs using Ruby.
+
+// -Identify two similarities and two differences between JavaScript and Ruby syntax with regard to 
+// numbers, arithmetic, strings, booleans, and various operators.
+//	For numbers is seems that both languages can use operators to perform arthmitic. Both languages
+// 	can manipulate either whole numbers or decimals. Ruby though, has two different classes for numbers,
+// 	and Integer class and Float class. JS just has a Number class for both.
+
+//	Whith strings, Both languages allow you to wrap them in "" or ''. Both use the \ to esacape
+//	characters. In Ruby you can perform string interpolation but with JS you only have the +.
+
+//	It looks like JS has some operators that are of written word rather than symbol, such as, typeof.
+//	I don't believe Ruby has this.
+
+//	Both use the booleans true and false but JS does have NaN (not a number) while Ruby would probably
+//	return an error instead.
+//	Both have the ternanry operator and use the || && ! logical operators.
+
+//	With undefined values Ruby will return nil but JS will return null or undefined.
+//	JS also has type coercion which will change certain values types. Ruby does not do this automatically.
+//	In Ruby the only false objects are nil and false but in JS 0, NaN, and ("") count as false in type coercion.
+//	With JS and using the == you can fall into type coercion traps so the alternative is to use ===. In Ruby,
+//	you only want to use the === if you need to find out if the two objects evaluate to the same object_id.
