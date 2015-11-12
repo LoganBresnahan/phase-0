4b06c09885da9b562d9bc2a7ea5c13ae4579ac53
Logan Bresnahan <loganbbres@gmail.com>
2015-11-11 18:50:18 -0600

    Finish Person 1 for 7.8

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,48 +1,10 @@
 /*
-1. As a user, I want to have two lists of numbers. One with an odd amount of numbers in it and one 
-   with an even amount of numbers in it.
+As a user, I want to create an action called sum that takes two different lists of numbers,
+which have already been made, and it will add them up separately.
 
-2. As a user, I want to make an action that takes three different guidelines. 
-   Some kind of test, a message I can send, and a number to list the number of tests I do.
-   As a user, I want my action to see if my test is legal or illegal. If my test is illegal I want to 
-   show on the screen the list number I am testing with the word "false" next to it, and I want
-   to show the word "ERROR: " with the message guideline on the screen. If my test is legal I
-   want to show the list number I am testing on the screen in front of the word "true" and I want
-   my program to asses as being legal. 
- 
-3. As a user, I want my first action to test if the total sum is a viable action, or in other words, will the total
-   sum work with my action. I want my message to say something like, "total sum should be a working action",
-   I want this to be my first action listed numerically when it shows on the screen.
+As a user, I want to create an action called mean that takes two different lists of numbers,
+which have already been made, and it will find the mean separately.
 
-4. As a user, I want to test if the total sum of the odd amount of numbers list is equal to 27, I want
-   the message to say something like, "the total sum should show the sum of all the numbers from the odd 
-   numbers list." This should be the second action listed numerically.
-
-5. As a user, I want to test if the total sum of the even amount of numbers list is equal to 43, I want
-   the message to say something like, "the total sum should show the sum of all the numbers from the even 
-   numbers list." This should be the third action listed numerically. 
-
-6. As a user, I want to test if the mean is a viable action, or in other words, will the mean
-   work with my action. I want the message to say something like, "mean should be a viable action."
-   This should be the forth action listed numerically.
-
-7. As a user, I want to test if the mean of the odd amount of numbers list is equal to 3.857142857142857
-   The message should say something like, "the mean should show the average of all the numbers from the odd
-   numbers list." This should be the fifth action listed numerically.
-
-8. As a user, I want to test if the mean of the even amount of numbers list is equal to 5.375
-    The message should say something like, "the mean should show the average of all the numbers from the even
-    numbers list." This should be the sixth action listed numerically.
-
-9. As a user, I want to test if the median is a viable action, or in other words, will the median
-    work with my action. I want the message to say something like, "the median should be a be a viable action."
-    This should be the seventh action listed numerically.
-
-10. As a user, I want to test if the median of the odd amount of numbers list is equal to 4.
-    The message should say something like, "the median should show the median of all the numbers from the odd 
-    numbers list." This should be the eighth action listed numerically.
-
-11. As a user, I want to test if the median of the even amount of numbers list is equal to 5.5.
-	The message should say something like, "the median should show the median of all the numbers from the even
-	numbers list." This should be the ninth action listed numerically.                
+As a user, I want to create an action called median that takes two different lists of numbers,
+which have already been made, and it will find the median separately.               
 */
